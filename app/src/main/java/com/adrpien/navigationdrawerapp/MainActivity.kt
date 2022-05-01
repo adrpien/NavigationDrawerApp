dfff15358cd83df54ecefc1d6cc810e8da2c8489
Adrian Pienkowski <adrpien@gmail.com>
2022-05-01 11:42:27 +0200

    Update

--- app/src/main/java/com/adrpien/navigationdrawerapp/MainActivity.kt
+++ app/src/main/java/com/adrpien/navigationdrawerapp/MainActivity.kt
@@ -7,6 +7,20 @@ import android.widget.Toast
 import com.adrpien.navigationdrawerapp.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
 
+/*
+How to create Navigation Drawer:
+1. Add depencies to build.gradle (module)
+2. Create menu XML drawer_menu.xml
+3. MainActivity.XML:
+   - Change root of your MainActivity layout. Set DrawerLayout as root.
+   - Add FrameLayout as container for your content
+   - Add NavigationView
+   - Change start position of Navigation View out of screen -
+     to do this set layout_gravity atribbute as start
+   - Add menu attribute to your navigation View
+4. Add setNavigationItemSelectedListener
+ */
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
